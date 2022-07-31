7b2c9b26fbe73e8b889d1cd2610d966c0b205992
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-31 19:19:33 -0400

    Update links.js

--- links.js
+++ links.js
@@ -1,6 +1,7 @@
 const data = [
   {name: "Github", link: "https://github.com/Name-shitty-github-profile"},
   {name: "Website", link: "https://website.notnoemie.repl.co"},
+  {name: "Mirador", link: "https://discord.gg/crouton"},
   {name: "Source", link: "https://github.com/Name-shitty-github-profile/link-website"}
 ];
 for (let i = 0; i < data.length; i++) {
